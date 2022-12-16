4a8c09d43deac88a8cced747c9e5b3e93e9dfa25
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-16 13:56:51 +0100

    ITERATION#4 : DONE

--- exercise.js
+++ exercise.js
@@ -80,3 +80,14 @@ for ( const place of placesToTravel) {
 
 console.log(" ");
 console.log("ITERATION#4---------------------------");
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for (const dato in alien) {
+    console.log("El dato: [", dato, "] del alien es:", alien[dato]);
+}
